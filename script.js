820a31b5e81ef7ab88be56a946bf22ccedb8643f
Shady-91 <Shady_91@hotmail.it>
2025-07-06 20:54:30 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// file javascript
+// This file is part of the js-biglietto-treno project.
+
+const priceKm = 0.21;
+let km = prompt("quanti km vuoi percorrere?");
+let age = prompt("quanti anni hai?");
+//prezzo al km e l'eta del passeggero
+const discountUnder18 = 20 %;
+//discount for under 18
+const discountOver65 = 40 %;
+//discount for over 65
